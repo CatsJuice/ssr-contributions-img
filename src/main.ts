fed492711100cdaa6fb803afd1f2bbabb2191916
CatsJuice <wp1298554944@hotmail.com>
2022-08-06 13:58:54 +0800

    chore(main): replace fastify-adpter with express

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,18 @@ import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+// import {
+//   FastifyAdapter,
+//   NestFastifyApplication,
+// } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const logger = new Logger('APP_BOOTSTRAP');
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  // const app = await NestFactory.create<NestFastifyApplication>(
+  //   AppModule,
+  //   new FastifyAdapter(),
+  // );
+  const app = await NestFactory.create(AppModule);
   const cfgSrv = app.get(ConfigService);
 
   await app.listen(parseInt(cfgSrv.get('SERVER_PORT')) || 3000, '::');
