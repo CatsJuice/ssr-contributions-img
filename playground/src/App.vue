a9e6883bf6eb5d4797f9a60180fc86030cdb0c8f
CatsJuice <wp1298554944@hotmail.com>
2022-08-12 21:59:52 +0800

    feat(playground): use adaptive layout to fit mobile screen

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,33 +1,29 @@
 <script setup lang="ts">
-import Display from './components/Display.vue';
-import Configuration from './components/Configuration.vue';
-import { useConfig } from './hooks/useConfig';
 import { watch } from 'vue';
 import { useQuasar } from 'quasar';
 
-const { darkMode } = useConfig();
+import Display from './components/Display.vue';
+import Configuration from './components/Configuration.vue';
+
+import { useConfig } from './hooks/useConfig';
+
+const { darkMode, isMobile } = useConfig();
 const $q = useQuasar();
 
-watch(
-  darkMode,
-  (darkMode) => {
-    $q.dark.set(darkMode);
-  },
-  { immediate: true },
-);
+watch(darkMode, (darkMode) => $q.dark.set(darkMode), { immediate: true });
 </script>
 
 <template>
-  <div class="fit row no-wrap items-center" :class="{ dark: darkMode }">
-    <Display id="display" class="col-grow fit-grow full-height"></Display>
-    <Configuration id="configuration" class="full-height"></Configuration>
+  <div
+    class="wrapper fit items-center"
+    :class="{ dark: darkMode, mobile: isMobile }"
+  >
+    <Display id="display"></Display>
+    <Configuration id="configuration"></Configuration>
   </div>
 </template>
 
 <style lang="scss">
-#display {
-}
-
 #configuration {
   width: 320px;
   flex-shrink: 0;
@@ -36,5 +32,59 @@ watch(
 }
 .dark #configuration {
   background-color: var(--q-dark);
+}
+
+.wrapper {
+  display: flex;
+  #display {
+    flex-grow: 1;
+    width: 0;
+  }
+  #display,
+  #configuration {
+    height: 100%;
+  }
+}
+
+@media screen and(max-width: 768px) {
+  #app {
+    height: fit-content !important;
+  }
+  body {
+    overflow-y: auto !important;
+  }
+  .wrapper.fit {
+    flex-direction: column;
+    height: fit-content !important;
+
+    #display {
+      width: 100%;
+      height: 50vh;
+      position: fixed;
+      left: 0;
+      top: 0;
+      z-index: 0;
+    }
+
+    #configuration {
+      width: 100%;
+      z-index: 1;
+      margin-top: 50vh;
+      height: fit-content;
+      display: block;
+      padding-bottom: 60px;
+
+      & > .col-grow.fit-grow {
+        height: fit-content;
+      }
+
+      /* & > footer {
+        background: inherit;
+        width: 100%;
+        position: fixed;
+        bottom: 0;
+      } */
+    }
+  }
 }
 </style>
